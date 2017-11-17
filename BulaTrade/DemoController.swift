d234196ca898471de7eae914f313a04be09362c6
Akash Verma <akash.verma@srmtechsol.com>
2017-11-17 16:51:23 +0530

    yes

--- BulaTrade/DemoController.swift
+++ BulaTrade/DemoController.swift
@@ -14,6 +14,7 @@ class DemoController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("now test plz")
     }
 
     override func didReceiveMemoryWarning() {
